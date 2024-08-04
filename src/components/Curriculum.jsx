d98a3a01e801c2f760b3c338f54b6a3797dd0aaf
joviscab <152897880+joviscab@users.noreply.github.com>
2024-08-04 06:00:03 +0200

    fix curriculum section info positioning

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -7,15 +7,15 @@ function Curriculum({ data }) {
       <p className="emailCV">Email: {data.email}</p>
       <p className="phoneCV">Phone: {data.phone}</p>
       <h2 className="workCV">Work Experience</h2>
-      <p>{data.experience}</p>
+      <p className="workContentCV">{data.experience}</p>
       <h2 className="academicCV">Academic Experience</h2>
-      <p>{data.education}</p>
+      <p className="academicContentCV">{data.education}</p>
       <h2 className="professionalCV">Professional Competencies</h2>
-      <p>{data.proComp}</p>
+      <p className="proContentCV">{data.proComp}</p>
       <h2 className="digitalCV">Digital Competencies</h2>
-      <p>{data.digComp}</p>
+      <p className="digContentCV">{data.digComp}</p>
       <h2 className="languageCV">Languages</h2>
-      <ul>
+      <ul className="langContentCV">
         {data.languages.map((language, index) => (
           <li key={index}>{language}</li>
         ))}
